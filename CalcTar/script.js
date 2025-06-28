747342c6f936306fb60876b0d201fe82bf61317e
Nathan Pinto <vihaan.nathan@gmail.com>
2025-06-28 19:17:30 +0530

    Update 2!

---
+++ CalcTar/script.js
@@ -0,0 +1,15 @@
+function addNumbers()
+                {
+                    const num1 = parseFloat(document.getElementById("input1").value);
+                    const num2 = parseFloat(document.getElementById("input2").value);
+                    const sum = num1 + num2;
+                    document.getElementById("result").innerText = "Result: "+ sum;
+
+                }
+                function subtractNumbers()
+                {
+                    const num1 = parseFloat(document.getElementById("input1").value);
+                    const num2 = parseFloat(document.getElementById("input2").value);
+                    const difference = num1 + num2;
+                    document.getElementById("result").innerText = "Result: "+ sum;
+                }
