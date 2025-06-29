30f285b9d9851c78ca34dd292e69034289922d81
Nathan Pinto <vihaan.nathan@gmail.com>
2025-06-29 07:58:20 +0530

    Update 4.7!

--- CalcTar/Arithmetic/script.js
+++ CalcTar/Arithmetic/script.js
@@ -4,13 +4,19 @@ function addNumbers()
                     const num2 = parseFloat(document.getElementById("input2").value);
                     const sum = num1 + num2;
                     document.getElementById("result").innerText = "Result: "+ sum;
+                    setTimeout(function(){},500);
+                    if(isNaN(num1) || isNaN(num2))
+                        document.getElementById("result").innerText = "Result: Invalid input!"
 
                 }
 function subtractNumbers()
                 {
                     const num1 = parseFloat(document.getElementById("input1").value);
                     const num2 = parseFloat(document.getElementById("input2").value);
-                    const difference = num1 + num2;
+                    const difference = num1 - num2;
                     document.getElementById("result").innerText = "Result: "+ difference;
+                    setTimeout(function(){},500);   
+                    if(isNaN(num1) || isNaN(num2))
+                        document.getElementById("result").innerText = "Result: Invalid input!"
                 }
-                /*This script has undergone the Update 3.3 Modifications! [latest]*/
+                /*This script has undergone the Update 4.7 Modifications! [latest]*/
